02b2c154df23574ba58e4b2d0acc9a3df3c1fba2
Samia <samia.abarcan@gmail.com>
2022-04-24 15:09:53 +0200

    Deleted the unnecesary gifts

--- adventjs02.js
+++ adventjs02.js
@@ -1,20 +1,22 @@
-const letter = "bici coche balón _playstation bici  coche peluche";
+const letter = "bici coche balón _playstation bici  coche  peluche"; //8
 
 function listGifts(letter) {
   // ¡Tú puedes!
   //////////////////////////////////////TODO
   var divided = letter.split(" ");
-
+  let i = 0;
   divided.forEach(value => {
-    console.log(value);
-
-      if(!value.length > 0 ){
-        console.log('ANTES ', divided);
-
-          divided.splice(0,1);
-          //console.log(value);
-          console.log('despues ', divided);
-      }
+    //console.log(value);
+    //console.log(value[0]);
+      if(value[0] === "_"){   
+        divided.splice(i,1);
+      }    
+      if (value === ''){
+        divided.splice(i,1);
+      } 
+      //console.log(i, value, "ESTAMOS");
+      divided.value = `${value}:1`;
+      i++;      
   });
 
 
