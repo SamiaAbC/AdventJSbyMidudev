01d01183ac9d5e8d43f3f8fcac0c6216af473242
Samia <samia.abarcan@gmail.com>
2022-05-02 23:02:09 +0200

    working on numbers

--- adventjs02.js
+++ adventjs02.js
@@ -7,13 +7,29 @@ function listGifts(letter) {
   //separate the array by the spaces
   var divided = letter.split(" ");
 
+  var pos = 0;
+  divided.forEach((value) =>{
+    if (divided[pos].length === 0)
+    {
+      divided.splice(pos,1);
+    }
+    else{
+      divided[pos] = `${divided[pos]}: 1`;
+      console.log(divided[pos], pos);
+console.log(value);
+      pos++;
+    }
+
+  } );
+console.log('DIVIDIDO', divided);
+
   for (var i = 0; i<divided.length; i++ )
   {
   //Remove empty spaces
-  if(divided[i].length === 0)
+ /*  if(divided[i].length === 0)
   {
     divided.splice(i,1);
-  }
+  } */
   if (divided[i].charAt(0) === '_')
   {
     divided.splice(i,1);
@@ -33,3 +49,4 @@ const regalos = listGifts(letter);
 console.log(regalos);
 
 //<<>
+
