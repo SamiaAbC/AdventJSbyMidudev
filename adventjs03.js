06aecc9c5799dc6d2da227289b44578e2699165a
Samia <samia.abarcan@gmail.com>
2022-05-22 00:34:50 +0200

    Starting Exercise 3

---
+++ adventjs03.js
@@ -0,0 +1,7 @@
+/* "bici coche (balón) bici coche peluche" // -> ✅
+"(muñeca) consola bici" // ✅
+
+"bici coche (balón bici coche" // -> ❌
+"peluche (bici [coche) bici coche balón" // -> ❌
+"(peluche {) bici" // -> ❌
+"() bici" // ❌ */
