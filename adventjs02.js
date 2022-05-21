9a4c2db08ce8a1c690f7e49c099ffbed9998df90
Samia <samia.abarcan@gmail.com>
2022-05-21 19:54:53 +0200

    Working on it

--- adventjs02.js
+++ adventjs02.js
@@ -5,28 +5,44 @@ function listGifts(letter) {
   //////////////////////////////////////TODO
   var divided = letter.split(" ");
   let i = 0;
+
   divided.forEach(value => {
-    //console.log(value);
-    //console.log(value[0]);
-      if(value[0] === "_"){   
+      if(value[0] === "_" || value === ''){   
         divided.splice(i,1);
-      }    
-      if (value === ''){
-        divided.splice(i,1);
-      } 
-      //console.log(i, value, "ESTAMOS");
-      divided.value = `${value}:1`;
+      }         
       i++;      
   });
+  
+  var res = [];
+  var repetidas;
+  for(let j = 0; j <= divided.length-1; j++)
+  {
+    repetidas = VecesRepetidas(divided[j], divided);
+    //Creo array de 'ya contado'
+    if(!res.includes(divided[j]))
+    {
+      res[j] = divided[j] + ": " + repetidas;
+      console.log("Hello");
+    }
+  }
 
+  return res;
+}
 
-  return divided;
+function VecesRepetidas(word, Array)
+{
+  var contador = 0;
+  for(let i = 0; i <= Array.length; i++)
+  {
+    if (Array[i] == word)
+    contador++;
+  }
+  return contador;
 }
 
 const regalos = listGifts(letter);
 
 console.log(regalos);
-
 
 //console.log(arr[0]); // "Soy"
 
